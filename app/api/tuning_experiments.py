803e69301c0d55ddff2959de53685853575fcbe0
Vunky Himawan <tingkerbeii44@gmail.com>
2026-06-08 19:27:01 +0700

    refactor(tuning-experiment): remove unused TuneParametersRequest model

--- app/api/tuning_experiments.py
+++ app/api/tuning_experiments.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
 
 from app.controllers.tuning_experiment_controller import TuningExperimentController
 from app.lib.logging.logging import get_logger
@@ -8,18 +7,12 @@ logger = get_logger(__name__)
 
 router = APIRouter(prefix="/tuning-experiments", tags=["Tuning Experiments"])
 
-
-class TuneParametersRequest(BaseModel):
-    file_path: str
-
-
 @router.post(
     path="/{tuning_experiment_dataset_id}",
     summary="Start search for optimal parameters for a given tuning experiment"
 )
 async def tune_parameters(
     tuning_experiment_dataset_id: str,
-    request: TuneParametersRequest,
 ):
     controller = TuningExperimentController()
     return await controller.tune_parameters(
